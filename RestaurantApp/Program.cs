ca25456db1e503f7252777cfafe4f424d050a736
ThanhTon123 <tonlogan11@gmail.com>
2020-12-11 13:33:32 +0700

    created Login and User View

--- RestaurantApp/Program.cs
+++ RestaurantApp/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using RestaurantApp.View;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -16,7 +17,8 @@ namespace RestaurantApp
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+
+            Application.Run(new User());
         }
     }
 }
